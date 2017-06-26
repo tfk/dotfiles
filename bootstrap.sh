0f616d5734d4e83fd52eeaac822dcc6a21bb10d2
Thomas Kunz <tfk@gmx.de>
2017-06-26 20:07:35 +0200

    Add --no-git option to skip git pull and submodule update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 # See https://github.com/mathiasbynens/dotfiles/blob/master/bootstrap.sh
 
-# Update from origin master
-git pull origin master 
+function doSync() {
+  rsync --exclude ".git/" \
+        --exclude ".gitignore" \
+        --exclude ".gitmodules" \
+        --exclude ".DS_Store" \
+        --exclude "README.md" \
+        --exclude "bootstrap.sh" \
+        --exclude ".vim/backup/*" \
+        --exclude ".vim/tmp/*" \
+        -avh --no-perms . ~;
+}
 
-# Update submodules 
-git submodule init && git submodule update
- 
-rsync --exclude ".git/" \
-      --exclude ".gitignore" \
-      --exclude ".gitmodules" \
-      --exclude ".DS_Store" \
-      --exclude "README.md" \
-      --exclude "bootstrap.sh" \
-      --exclude ".vim/backup/*" \
-      --exclude ".vim/tmp/*" \
-      -avh --no-perms . ~;
+cd "$(dirname "${BASH_SOURCE}")";
 
+if [ "$1" == "--no-git" ]; then
+  doSync;
+else
+  # Update from origin master
+  git pull origin master 
+  # Update submodules 
+  git submodule init && git submodule update
 
+  doSync;
+fi
